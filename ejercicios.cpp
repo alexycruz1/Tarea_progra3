f8a8a77a61062be9b12e056d80864cbd00ad1a12
alexycruz1 <xytojr@gmail.com>
2016-02-20 19:12:59 -0600

    haciendo ejercicio numero 16

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
 int menu();
+double PowerDigitSum(double, double);
 
 int main(int argc, char*argv[]){
 
 	if (menu() == 1){
-		cout << "prueba1";
+		double base, expo;
+		cout << endl;
+
+		cout << "Ingrese una base: ";
+		cin >> base;
+
+		cout << "Ingrese una potencia: ";
+		cin >> expo;
+		cout << endl;
+
+		PowerDigitSum(base, expo);
+
 	}else if (menu() == 2){
 		cout << "prueba2";
 	}else if (menu() == 3){
@@ -29,6 +43,14 @@ int menu(){
 	cin >> opcion; 
 
 	return opcion;
+}
+
+double PowerDigitSum(double base, double expo){//no esta terminado
+	double exponencial = pow(base, expo);
+
+	cout << resultado;
+
+	return resultado;
 }
 
 
